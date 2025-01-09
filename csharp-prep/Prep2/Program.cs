b4314eee9e933c4761782cd0ef3390e311eab21f
Tyler Hall <hal24007@byui.edu>
2025-01-09 15:29:35 -0700

    Prove assignment 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentageGrade = Console.ReadLine();
+        string letterGrade;
+
+        if (int.Parse(percentageGrade) >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (int.Parse(percentageGrade) >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (int.Parse(percentageGrade) >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (int.Parse(percentageGrade) >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine(letterGrade);
+
+        if (int.Parse(percentageGrade) >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
     }
 }
