965c515962918c813aafe64d9d021083f8964866
Tyler Hall <hal24007@byui.edu>
2025-01-21 17:05:47 -0700

    W03 Prepare

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning02
+{
+    internal class Resume
+    {
+        public string _name;
+        public List<Job> _jobs = new List<Job>();
+        
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs:");
+
+            foreach (Job job in _jobs)
+            {
+                job.Display();
+            }
+        }
+    }
+}
